55399df23a9b5b085fb1a4067d2dbae90d0d698a
Erfan Navab <erfannavab@gmail.com>
2023-12-16 14:24:15 +0330

    added database information

---
+++ src/main/java/nvb/dev/bookmanagementjsp/connection/JdbcConstant.java
@@ -0,0 +1,12 @@
+package nvb.dev.bookmanagementjsp.connection;
+
+public class JdbcConstant {
+
+    private JdbcConstant() {
+    }
+
+    public static final String URL = "jdbc:postgresql://localhost:5432/book-jsp-db";
+    public static final String USER = "postgres";
+    public static final String PASSWORD = "123456789Erfan";
+
+}
