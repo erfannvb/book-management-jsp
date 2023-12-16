554b306c09c0bdb13ff333ccf2bec28498af194c
Erfan Navab <erfannavab@gmail.com>
2023-12-16 14:24:34 +0330

    configured jdbc

---
+++ src/main/java/nvb/dev/bookmanagementjsp/connection/JdbcConnection.java
@@ -0,0 +1,49 @@
+package nvb.dev.bookmanagementjsp.connection;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static nvb.dev.bookmanagementjsp.connection.JdbcConstant.*;
+
+public class JdbcConnection {
+
+    private JdbcConnection() {
+    }
+
+    private static final HikariDataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(URL);
+        config.setUsername(USER);
+        config.setPassword(PASSWORD);
+        config.setMaximumPoolSize(10);
+        config.setAutoCommit(true);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static void closeConnection(Connection connection) throws SQLException {
+        if (connection != null) connection.close();
+    }
+
+    public static void closePreparedStatement(PreparedStatement preparedStatement) throws SQLException {
+        if (preparedStatement != null) preparedStatement.close();
+    }
+
+    public static void closeResultSet(ResultSet resultSet) throws SQLException {
+        if (resultSet != null) resultSet.close();
+    }
+
+}
