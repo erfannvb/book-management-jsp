c576e9453f38bcb3c998d3b286671f16e9671ce0
Erfan Navab <erfannavab@gmail.com>
2023-12-20 17:06:39 +0330

    added javascript file

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("register-btn").addEventListener("click", () => {
+    window.location.href = "/register.jsp";
+})
+
+document.getElementById("login-btn").addEventListener("click", () => {
+    window.location.href = "/login.jsp";
+})
