87b136103259da6412d6aeff883094fc9e2e402f
Erfan Navab <erfannavab@gmail.com>
2023-12-20 17:06:17 +0330

    added user entity

---
+++ src/main/java/nvb/dev/bookmanagementjsp/entity/User.java
@@ -0,0 +1,28 @@
+package nvb.dev.bookmanagementjsp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User implements Serializable {
+
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String password;
+
+    public User(String username, String firstName, String lastName, int age, String password) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.password = password;
+    }
+}
