c4927f098235d54051eb96f05751a9466de0ee72
Erfan Navab <erfannavab@gmail.com>
2023-12-21 14:41:18 +0330

    removed unused imports

--- src/main/java/nvb/dev/bookmanagementjsp/servlet/AddBookServlet.java
+++ src/main/java/nvb/dev/bookmanagementjsp/servlet/AddBookServlet.java
@@ -25,11 +25,6 @@ public class AddBookServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-    }
-
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
 
