aa16f909ed0a64e1bc588297995be580df9acfee
Erfan Navab <erfannavab@gmail.com>
2023-12-26 23:55:53 +0330

    added the ability that checks if username exists in the db

--- src/main/java/nvb/dev/bookmanagementjsp/filter/RegisterUserFilter.java
+++ src/main/java/nvb/dev/bookmanagementjsp/filter/RegisterUserFilter.java
@@ -2,8 +2,14 @@ package nvb.dev.bookmanagementjsp.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nvb.dev.bookmanagementjsp.entity.User;
+import nvb.dev.bookmanagementjsp.repository.UserRepository;
+import nvb.dev.bookmanagementjsp.repository.impl.UserRepositoryImpl;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebFilter(filterName = "RegisterUserFilter", servletNames = "RegisterUserServlet", urlPatterns = "/register")
 public class RegisterUserFilter implements Filter {
@@ -11,23 +17,36 @@ public class RegisterUserFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
 
-        String username = servletRequest.getParameter("username");
-        String firstName = servletRequest.getParameter("firstName");
-        String lastName = servletRequest.getParameter("lastName");
-        String age = servletRequest.getParameter("age");
-        String password = servletRequest.getParameter("pwd");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        UserRepository userRepository = new UserRepositoryImpl();
+        List<User> userList = userRepository.getAllUsers();
+
+        String username = request.getParameter("username");
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String age = request.getParameter("age");
+        String password = request.getParameter("pwd");
 
         StringBuilder error = new StringBuilder();
 
-        if (username == null || username.isBlank() || firstName == null || firstName.isBlank() ||
-                lastName == null || lastName.isBlank() || age == null || age.isBlank() ||
-                password == null || password.isBlank()) {
-            error.append("Fill in all the fields.");
-            servletRequest.setAttribute("error", error);
-            RequestDispatcher dispatcher = servletRequest.getRequestDispatcher("/register.jsp");
-            dispatcher.include(servletRequest, servletResponse);
-        } else {
-            filterChain.doFilter(servletRequest, servletResponse);
+        for (User user : userList) {
+            if (user.getUsername().equals(username)) {
+                error.append("User with this username already exists!");
+                servletRequest.setAttribute("error", error);
+                RequestDispatcher dispatcher = servletRequest.getRequestDispatcher("/register.jsp");
+                dispatcher.include(request, response);
+            } else if (username == null || username.isBlank() || firstName == null || firstName.isBlank() ||
+                    lastName == null || lastName.isBlank() || age == null || age.isBlank() ||
+                    password == null || password.isBlank()) {
+                error.append("Fill in all the fields.");
+                servletRequest.setAttribute("error", error);
+                RequestDispatcher dispatcher = servletRequest.getRequestDispatcher("/register.jsp");
+                dispatcher.include(request, response);
+            } else {
+                filterChain.doFilter(request, response);
+            }
         }
 
     }
